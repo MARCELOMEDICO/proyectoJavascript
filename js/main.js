68e42505b5841a45e8a79da4da84bbbde26a036f
marcelo medico <marcelot217@gmail.com>
2022-08-06 00:59:25 -0400

    agregando array

--- js/main.js
+++ js/main.js
@@ -5,35 +5,40 @@ function getData2(){
     let numeros = +document.getElementById("numerosUnidad").value;
     let largo = document.getElementById("largo").value;
     let peso =+document.getElementById("pesoTarro").value;
+    
 
-    if (letras =="") {
-        alert("INGRESE LETRAS")
-        letras =document.getElementById("letrasUnidad").focus();
-    }else 
-        if(numeros ==""){
-            alert("ingrese valor a NUMEROS")
-            numeros=+document.getElementById("numerosUnidad").focus();
-        }else 
-            if(largo ==""){
-            alert("ingrese MEDIDA")
-            largo = document.getElementById("largo").focus();
-        }else 
-            if(peso==""){
-                alert("ingrese PESO DEL TARRO")
-                peso =+document.getElementById("pesoTarro").focus();
-            }
-
-    ///esta parte es un objeto--- const con llaves"
-    const tarro={
-        fecha: document.getElementById("fechaIngreso").value,
-        movimiento : document.getElementById("movimiento").value,
-        letras : document.getElementById("letrasUnidad").value,
-        numeros : +document.getElementById("numerosUnidad").value,
-        largo : document.getElementById("largo").value,
-        peso :+document.getElementById("pesoTarro").value,
-    }
-    console.log(tarro)
+    // if (letras =="") {
+    //     alert("INGRESE LETRAS")
+    //     letras =document.getElementById("letrasUnidad").focus();
+    // }else 
+    //     if(numeros ==""){
+    //         alert("ingrese valor a NUMEROS")
+    //         numeros=+document.getElementById("numerosUnidad").focus();
+    //     }else 
+    //         if(largo ==""){
+    //         alert("ingrese MEDIDA")
+    //         largo = document.getElementById("largo").focus();
+    //     }else 
+    //         if(peso==""){
+    //             alert("ingrese PESO DEL TARRO")
+    //             peso =+document.getElementById("pesoTarro").focus();
+    //         }
+    const listaTarros =[]
+    const tarroNew=[
+        fecha= document.getElementById("fechaIngreso").value,
+        movimiento = document.getElementById("movimiento").value,
+        letras = document.getElementById("letrasUnidad").value,
+        numeros = +document.getElementById("numerosUnidad").value,
+        largo = document.getElementById("largo").value,
+        peso =+document.getElementById("pesoTarro").value,
+    ]
+    listaTarros.push(fecha,movimiento,letras,numeros,largo,peso)
+    console.log(listaTarros)
+    
+    
+    
 }
+    
 
 
 
